1d67e829f76e726a188393d1ac8e5b143375e3b4
Brian Gannon <briancgannon@gmail.com>
2019-03-29 14:45:44 -0400

    wip

--- create-project.py
+++ create-project.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env python3
 
+from azure.devops.connection import Connection
+from msrest.authentication import BasicAuthentication
+import pprint
 import optparse
 
 parser = optparse.OptionParser()
 
 parser.add_option("-d", "--dry-run",
     action="store", dest="dry-run",
-    help="Perform dry-run", default=False)
+    help="perform dry-run. default=false", default=False)
 
 parser.add_option("-t", "--token",
     action="store", dest="token",
-    help="Input ADO Personal Access Token", default="ProvidePersonalAccessToken")
+    help="ADO Personal Access Token")
 
 parser.add_option("-o", "--organization",
     action="store", dest="organization",
-    help="Input ADO organization")
-
+    help="ADO organization")
 
 (options, args) = parser.parse_args()
 
+if not options.token:
+    parser.error('Please provide an Azure DevOps Personal Access Token')
+elif not options.organization:
+    parser.error('Please provide an Azure DevOps Organization')
+
+if options.organization:
+    organization_url = 'https://dev.azure.com/' + options.organization
+else:
+    organization_url = 'https://dev.azure.com/'
+
+
+print('Starting Azure DevOps Project creation with the following options: ')
+print('Token: ', options.token)
+print('Organization: ', organization_url)
+
+# Create a connection
+credentials = BasicAuthentication('', options.token)
+connection = Connection(base_url=organization_url, creds=credentials)
+
+# Get a client
+core_client = connection.clients.get_core_client()
+
+# Get a list of the projects in the org
+projects = core_client.get_projects()
+
+# Show projects
+for project in projects:
+    pprint.pprint(project.__dict__)
 
