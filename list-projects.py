117a91ac995ebb53df90c82916035b45be03d513
Brian Gannon <briancgannon@gmail.com>
2019-04-02 09:23:31 -0400

    wip

--- list-projects.py
+++ list-projects.py
@@ -17,3 +17,27 @@ parser.add_option("-o", "--organization",
 
 (options, args) = parser.parse_args()
 
+if not options.token:
+    parser.error('Please provide an Azure DevOps Personal Access Token')
+elif not options.organization:
+    parser.error('Please provide an Azure DevOps Organization')
+
+if options.organization:
+    organization_url = 'https://dev.azure.com/' + options.organization
+else:
+    organization_url = 'https://dev.azure.com/'
+
+# Create a connection
+credentials = BasicAuthentication('', options.token)
+connection = Connection(base_url=organization_url, creds=credentials)
+
+# Get a client
+core_client = connection.clients.get_core_client()
+
+# Get a list of the projects in the org
+projects = core_client.get_projects()
+
+# Show projects
+for project in projects:
+    pprint.pprint(project.__dict__)
+
