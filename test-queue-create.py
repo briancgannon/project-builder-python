20a30d41a3f62f9fa3aef22a65bdb18c1cdc3657
Brian Gannon <briancgannon@gmail.com>
2019-04-05 09:45:52 -0400

    clean up

--- test-queue-create.py
+++ test-queue-create.py
@@ -10,6 +10,7 @@ organization_url = 'https://dev.azure.com/gannonbrian'
 personal_access_token = os.getenv('AZURE_DEVOPS_PAT')
 my_project_id = "test-01"
 my_project_name = "test-project"
+my_project_description = "This is garbage."
 
 # ADO stuff
 credentials = BasicAuthentication('', personal_access_token)
@@ -20,11 +21,12 @@ def create_ado_project(id, name, description):
     core_client.queue_create_project(abbreviation="MTP",
         description="Test Project",
         name="test-project",
-        id="test-project",
+        id="tp-01",
         capabilities={'process-template':'agile',
             'version-control':'git'}
             )
 
 # TODO: Until create status = wellFormed
+resp = requests.get('https://dev.azure.com/gannonbrian/_apis/operations/tp-01/?api-version=5.0')
+create_ado_project(my_project_id, my_project_name, my_project_description)
 
-resp = create_ado_project(my_project_id, my_project_name)
