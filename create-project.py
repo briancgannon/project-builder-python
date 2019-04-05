bfe2b3e061890423d8d558a986e25c8af92db97f
Brian Gannon <briancgannon@gmail.com>
2019-04-05 13:40:23 -0400

    wip

--- create-project.py
+++ create-project.py
@@ -9,14 +9,13 @@ import requests
 organization_url = 'https://dev.azure.com/gannonbrian'
 personal_access_token = os.getenv('AZURE_DEVOPS_PAT')
 my_project_name = "test-project"
-my_project_description = "This is garbage."
 
 # ADO stuff
 credentials = BasicAuthentication('', personal_access_token)
 connection = Connection(base_url=organization_url, creds=credentials)
 core_client = connection.clients.get_core_client()
 
-def create_project(name, description):
-    core_client.queue_create_project(name=name,
-        description=description,
-        )
+def create_project(name):
+    core_client.queue_create_project(project_to_create=name)
+
+create_project(my_project_name)
